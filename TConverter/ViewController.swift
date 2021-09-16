647a98027a23adfc2cbc85b2eaf6b97faaeefc64
odgigodji <AZM-snn-J9f-G2L>
2021-09-16 18:53:43 +0300

    logic with positive only

--- TConverter/ViewController.swift
+++ TConverter/ViewController.swift
@@ -10,17 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var celiusLabel: UILabel!
+    @IBOutlet weak var celsiusLabel: UILabel!
     @IBOutlet weak var farenheitLabel: UILabel!
-    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+        }
+    }
     
     @IBAction func sliderChanged(_ sender: UISlider) {
-//        for _ in slider.
+        let temperatureC = Int(round(sender.value))
+        celsiusLabel.text = "\(temperatureC)ºC"
+        
+        let temperatureF = Int(round((sender.value * 9 / 5) + 32))
+        farenheitLabel.text = "\(temperatureF)ºF"
     }
     
 
 }
 
 /*
+ about Size Classes:
  https://developer.apple.com/design/human-interface-guidelines/ios/visual-design/adaptivity-and-layout/
+ Compact/Regular width, height
  */
